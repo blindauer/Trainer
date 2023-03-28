e07690404d4bf3c8bfd8688c5d8c45dbbd4143ba
Bradley Lindauer <blindauer@mac.com>
2023-03-28 00:23:28 -0700

    Initial Commit

---
+++ Trainer/TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainerApp.swift
+//  Trainer
+//
+//  Created by Bradley Lindauer on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
